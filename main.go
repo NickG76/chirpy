0841a8b5cbc3f7c7f4bc073d1d6c5589cc8689bf
NickG76 <nickgirling76@gmail.com>
2025-07-17 13:19:55 +0100

    updated alot *sweats*

--- main.go
+++ main.go
@@ -8,16 +8,21 @@ import (
 	"os"
 	"database/sql"
 	"github.com/joho/godotenv"
+	"github.com/nickg76/chirpy/internal/database"
 
 	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	DB *database.Queries
 }
 
 func main() {
-	gotdotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env files, do they exist?")
+	}
 	const filepathRoot = "./app/"
 	const port = "8080"
 
@@ -29,9 +34,9 @@ func main() {
 	defer db.Close()
 	dbQueries := database.New(db)
 
-
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		DB: dbQueries,
 	}
 
 	mux := http.NewServeMux()
@@ -40,14 +45,15 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("/api/validate_chirp", apiCfg.validateChirpHandler)
+	mux.HandleFunc("POST /api/users", apiCfg.handleCreateUsr)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	fmt.Println()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	fmt.Println()
 	log.Printf("Server running on: http://localhost:%s/app/\n", port)
 	log.Fatal(srv.ListenAndServe())
